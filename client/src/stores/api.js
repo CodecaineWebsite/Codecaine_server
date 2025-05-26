d970657d305f1945839ad682d5bfe8b32e8b4adc
wey <45823398+WeyTing@users.noreply.github.com>
2025-05-26 21:48:44 +0800

    [fix]調整token邏輯

--- client/src/stores/api.js
+++ client/src/stores/api.js
@@ -1,20 +1,22 @@
 import axios from "axios";
-import { useAuthStore } from "../stores/useAuthStore";
+import { getAuth } from "firebase/auth";
 
 const api = axios.create({
   baseURL: "http://localhost:3000/api", // 預設 base URL
 });
 
 api.interceptors.request.use(
-  (config) => {
-    const authStore = useAuthStore();
-    const token = authStore.idToken;
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
+  async (config) => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      const token = await user.getIdToken(); // 不要加參數 true，會自動 refresh 過期 token
+      config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => Promise.reject(error)
 );
-
 export default api;
