793a26da6c724fbe5e97cbacff0167a1b559435f
wey <45823398+WeyTing@users.noreply.github.com>
2025-05-26 23:21:59 +0800

    [feat]完成profile基礎頁面架構

---
+++ client/src/layouts/ProfileLayout.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>
+      <header class="profile-header">
+        <div class="profile-info">
+          <h1>Profile</h1>
+          <button @click="Cains">Cains</button>
+          <button @click="Following">Following</button>
+          <button @click="Followers">Followers</button>
+        </div>
+      </header>
+    </div>
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const Cains = () => {
+  router.push("/profile/cains");
+};
+const Following = () => {
+  router.push("/profile/following");
+};
+const Followers = () => {
+  router.push("/profile/followers");
+};
+</script>
+
+<style scoped>
+.profile-header {
+  background-color: #131417;
+  color: white;
+  padding: 20px;
+  border-bottom: 3px solid #2c303a;
+}
+button {
+  background-color: #05df72;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  cursor: pointer;
+}
+</style>
---
+++ client/src/views/Cains.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>Cains</h1>
+  <div>
+    <button @click="showcase">Showcase</button>
+    <button @click="publicProfile">Public</button>
+    <button @click="loved">Loved</button>
+    <button @click="privateProfile">Private</button>
+  </div>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const showcase = () => {
+  router.push("/profile/cains/showcase");
+};
+const publicProfile = () => {
+  router.push("/profile/cains/public");
+};
+const loved = () => {
+  router.push("/profile/cains/loved");
+};
+const privateProfile = () => {
+  router.push("/profile/cains/private");
+};
+</script>
+
+<style scoped>
+button {
+  background-color: #05df72;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  cursor: pointer;
+}
+</style>
