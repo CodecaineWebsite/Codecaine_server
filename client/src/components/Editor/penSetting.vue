82d571e14620dcf4fe4565bd9ef84fca4ed9023b
Barry <barry890426@gmail.com>
2025-05-31 20:25:54 +0800

    [fix] penSetting overflow fixed

--- client/src/components/Editor/penSetting.vue
+++ client/src/components/Editor/penSetting.vue
@@ -82,30 +82,31 @@ const removeLink = (index) => {
 
 </script>
 <template>
-  <div class="fixed md:translate-y-4/7 translate-y-1/2 left-1/2 -translate-x-1/2 md:h-4/5 h-full md:w-175 max-w-185 w-full pb-20 px-4">
-  <div  class=" flex flex-col rounded-sm  bg-[#131417] text-white border-4 border-gray-600 h-full w-full" >
-    <div class="mx-4 pt-4">
-      <div class="flex justify-between">
-        <span class="after:content-[''] after:border-b-2 after:border-green-400 after:absolute after:-bottom-0.5 after:left-0 after:w-full relative pb-1">Pen Settings</span>
-        <button type="button" @click.prevent="emit('close')" class="right-1 bg-[#424655] w-7 flex justify-center h-4">
-          <svg class="w-2.5" viewBox="0 0 100 100">
-            <path fill="white"
-              d="M96.8 83.7L63.1 50l33.7-33.7c3.6-3.6 3.6-9.4 0-13.1s-9.5-3.6-13.1 0L50 36.9 16.3 3.2C12.7-.4 6.9-.4 3.2 3.2s-3.6 9.5 0 13.1L36.9 50 3.2 83.7c-3.6 3.6-3.6 9.4 0 13.1s9.5 3.6 13.1 0L50 63.1l33.7 33.7c3.6 3.6 9.4 3.6 13.1 0s3.6-9.5 0-13.1z">
-            </path>
-          </svg>
-        </button>
-      </div>
+  <div class="fixed md:translate-y-4/7 translate-y-1/2 left-1/2 -translate-x-1/2 md:h-4/5 h-11/12 md:w-175 max-w-185 w-full pb-20 px-4">
+    <div  class=" flex flex-col rounded-sm  bg-[#131417] text-white border-4 border-gray-600 h-full w-full " >
+      <div class="mx-4 pt-4">
+        <div class="flex justify-between">
+          <span class="after:content-[''] after:border-b-2 after:border-green-400 after:absolute after:-bottom-0.5 after:left-0 after:w-full relative pb-1">Pen Settings</span>
+          <button type="button" @click.prevent="emit('close')" class="right-1 bg-[#424655] w-7 flex justify-center h-4">
+            <svg class="w-2.5" viewBox="0 0 100 100">
+              <path fill="white"
+                d="M96.8 83.7L63.1 50l33.7-33.7c3.6-3.6 3.6-9.4 0-13.1s-9.5-3.6-13.1 0L50 36.9 16.3 3.2C12.7-.4 6.9-.4 3.2 3.2s-3.6 9.5 0 13.1L36.9 50 3.2 83.7c-3.6 3.6-3.6 9.4 0 13.1s9.5 3.6 13.1 0L50 63.1l33.7 33.7c3.6 3.6 9.4 3.6 13.1 0s3.6-9.5 0-13.1z">
+              </path>
+            </svg>
+          </button>
+        </div>
         <div class="w-full h-0.5 bg-gray-600 mb-4"></div>
       </div>
-        <div class="md:flex h-full mx-4 block">
-          <ul class="md:w-1/4 flex md:flex-col overflow-scroll md:overflow-visible pl-2 md:pl-0">
-            <li v-for="tab in tabs" :key="tab.key" tabindex="0" @click.prevent="activeTab = tab.key"  class="whitespace-nowrap transition hover:bg-gray-600 px-1.5 md:pl-4 ml-1 md:ml-0 relative -left-4 before:content-none md:before:content-['']  before:absolute before:w-1 before:h-full before:left-0 focus:before:bg-green-500" :class="{ 'before:bg-green-500': activeTab === tab.key, 'md:mt-4': tab.gapBefore,  'bg-gray-600': activeTab === tab.key}">
-              {{  tab.label }}
-            </li>
-          </ul>
-          <div class="md:hidden w-full flex my-3 md:before:content-none before:content-[''] before:relative before:w-full before:h-0.5 before:bg-gray-700 before:mt-2"></div>
+      <div class="md:flex h-full px-4 block overflow-y-auto ">
+        <ul class="md:w-1/4 flex md:flex-col md:overflow-y-auto pl-2 md:pl-0">
+          <li v-for="tab in tabs" :key="tab.key" tabindex="0" @click.prevent="activeTab = tab.key"  class="whitespace-nowrap transition hover:bg-gray-600 px-1.5 md:pl-4 ml-1 md:ml-0 relative -left-4 before:content-none md:before:content-['']  before:absolute before:w-1 before:h-full before:left-0 focus:before:bg-green-500" :class="{ 'before:bg-green-500': activeTab === tab.key, 'md:mt-4': tab.gapBefore,  'bg-gray-600': activeTab === tab.key}">
+            {{  tab.label }}
+          </li>
+        </ul>
+        <div class="md:hidden w-full flex my-3 md:before:content-none before:content-[''] before:relative before:w-full before:h-0.5 before:bg-gray-700 before:mt-2"></div>
+        <div class="md:w-3/4 w-full h-11/12 overflow-y-auto">
 
-          <div v-show="activeTab === 'html'" class="md:w-3/4 w-full flex flex-col gap-4">
+          <div v-show="activeTab === 'html'" class=" w-full flex flex-col gap-4">
             <div class="relative editorSettingCard-linear-bgc py-3 px-4 w-full before:h-full before:w-1 before:bg-gray-500 before:content-[''] before:absolute before:top-0 before:left-0">
               <div class="">
                 <label for="HTML Preprocessor">HTML Preprocessor</label>
@@ -147,7 +148,7 @@ const removeLink = (index) => {
             </div>
           </div>
 
-          <div v-show="activeTab === 'css'" class="md:w-3/4 w-full flex flex-col gap-4">
+          <div v-show="activeTab === 'css'" class=" w-full flex flex-col gap-4">
             <div class="relative editorSettingCard-linear-bgc py-3 px-4 w-full before:h-full before:w-1 before:bg-gray-500 before:content-[''] before:absolute before:top-0 before:left-0">
               <div class="">
                 <label for="CSS Preprocessor">CSS Preprocessor</label>
@@ -225,7 +226,7 @@ const removeLink = (index) => {
             </div>
           </div>
 
-          <div v-show="activeTab === 'js'" class="md:w-3/4 w-full flex flex-col gap-4">
+          <div v-show="activeTab === 'js'" class=" w-full flex flex-col gap-4">
             <div class="relative editorSettingCard-linear-bgc py-3 px-4 w-full before:h-full before:w-1 before:bg-gray-500 before:content-[''] before:absolute before:top-0 before:left-0">
               <div class="">
                 <label for="JavaScript Preprocessor">JavaScript Preprocessor</label>
@@ -262,7 +263,7 @@ const removeLink = (index) => {
             </div>
           </div>
           
-          <div v-show="activeTab === 'detail'" class="md:w-3/4 w-full flex flex-col gap-4">
+          <div v-show="activeTab === 'detail'" class=" w-full flex flex-col gap-4">
             <div class="relative editorSettingCard-linear-bgc py-3 px-4 w-full before:h-full before:w-1 before:bg-gray-500 before:content-[''] before:absolute before:top-0 before:left-0">
               <div class="">
                 <label for="CSS Preprocessor">CSS Preprocessor</label>
@@ -290,10 +291,11 @@ const removeLink = (index) => {
             </div>
           </div>
         </div>
-        <div class="bg-gray-600 w-full flex flex-col py-4 px-2">
-          <button type="submit" @click.prevent="emit('close')" class="self-end bg-green-400 text-black rounded-md p-3">Save & Close</button>
-        </div>
       </div>
+    </div>
+    <div class="bg-gray-600 w-full flex flex-col py-4 px-2 ">
+    <button type="submit" @click.prevent="emit('close')" class="self-end bg-green-400 text-black rounded-md p-3">Save & Close</button>
+    </div>
   </div>
 
 </template>
