547e1db948151972bbef1406f7c02a5ad75982d9
wey <qaz237562@gmail.com>
2025-06-16 12:12:39 +0800

    [feat]新增 usersCaines 路由以取得使用者作品，並整理 index.js 檔案的匯入與格式

--- index.js
+++ index.js
@@ -10,16 +10,17 @@ import favoritesRouter from "./src/routes/favorites.js";
 import commentsRouter from "./src/routes/comments.js";
 import followsRouter from "./src/routes/follows.js";
 import searchRouter from "./src/routes/search.js";
-import yourWorkRouter from "./src/routes/yourWork.js"
-import trendingRouter from "./src/routes/trending.js"
+import yourWorkRouter from "./src/routes/yourWork.js";
+import trendingRouter from "./src/routes/trending.js";
 import followingRouter from "./src/routes/following.js";
+import usersCainesRouter from "./src/routes/usersCaines.js";
 const PORT = 3000;
 
 const app = express();
 app.use(
-	cors({
-		origin: "http://localhost:5173",
-	})
+  cors({
+    origin: "http://localhost:5173",
+  })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -32,10 +33,11 @@ app.use("/api/favorites", favoritesRouter);
 app.use("/api/comments", commentsRouter);
 app.use("/api/follows", followsRouter);
 app.use("/api/search", searchRouter);
-app.use("/api/following",followingRouter);
-app.use("/api/trending",trendingRouter)
-app.use("/api/my",yourWorkRouter);
+app.use("/api/following", followingRouter);
+app.use("/api/trending", trendingRouter);
+app.use("/api/my", yourWorkRouter);
+app.use("/api/usersCaines", usersCainesRouter);
 
 app.listen(PORT, () => {
-	console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
