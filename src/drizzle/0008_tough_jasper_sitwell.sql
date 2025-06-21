9aa92090a610c3a41533da70a5cf5116dac7590e
chinyuting <s86052915@gmail.com>
2025-06-21 14:08:19 +0800

    [feat] generate and migrate new DB schema

---
+++ src/drizzle/0008_tough_jasper_sitwell.sql
@@ -0,0 +1,21 @@
+CREATE TYPE "public"."role" AS ENUM('user', 'assistant');--> statement-breakpoint
+CREATE TABLE "ai_chats" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "ai_chats_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"title" varchar(100) DEFAULT 'untitled',
+	"created_at" timestamp with time zone DEFAULT now(),
+	"user_id" varchar(128),
+	"pen_id" integer NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "ai_messages" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "ai_messages_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"chat_id" integer NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"content" text NOT NULL,
+	"role" "role" NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "ai_chats" ADD CONSTRAINT "ai_chats_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "ai_chats" ADD CONSTRAINT "ai_chats_pen_id_pens_id_fk" FOREIGN KEY ("pen_id") REFERENCES "public"."pens"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "ai_messages" ADD CONSTRAINT "ai_messages_chat_id_ai_chats_id_fk" FOREIGN KEY ("chat_id") REFERENCES "public"."ai_chats"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "chat_id_idx" ON "ai_messages" USING btree ("chat_id");
