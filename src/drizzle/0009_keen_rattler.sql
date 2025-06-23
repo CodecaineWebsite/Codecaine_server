099dc666424789f90012560fc30e0e4d85b35221
chinyuting <s86052915@gmail.com>
2025-06-24 00:51:30 +0800

    [chore] remove Drizzle ORM files

--- src/drizzle/0009_keen_rattler.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "ai_chats" ALTER COLUMN "title" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "ai_messages" ALTER COLUMN "content" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "ai_chats" ADD COLUMN "status" integer DEFAULT 1 NOT NULL;--> statement-breakpoint
-ALTER TABLE "ai_messages" ADD COLUMN "status" integer DEFAULT 1 NOT NULL;--> statement-breakpoint
-ALTER TABLE "ai_messages" ADD COLUMN "message_index" integer NOT NULL;
--- src/drizzle/0010_last_susan_delgado.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "ai_chats" DROP CONSTRAINT "ai_chats_pen_id_pens_id_fk";
---> statement-breakpoint
-ALTER TABLE "ai_chats" DROP COLUMN "pen_id";
